f79f814f2a0eec309c16baa24dc93d09b3c71f60
Narendra <scriptstar@hotmail.co.uk>
2020-04-28 12:34:59 +0100

    changes

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,5 +7,5 @@ set -v
 curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/pull > /dev/null
 npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ea8030851543d00196ae33a
 curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/ssgbuild > /dev/null
-hugo --gc --baseURL "/"
+hugo
 curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/publish > /dev/null
