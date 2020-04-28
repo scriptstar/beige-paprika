1348ea9e4b4a8baf7f65de9b5b10d713537d6809
Stackbit <projects@stackbit.com>
2020-04-28 10:20:21 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/pull > /dev/null
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ea8030851543d00196ae33a
+curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/ssgbuild > /dev/null
+hugo --gc --baseURL "/" --themesDir ../.. && cd ..
+curl -s -X POST https://api.stackbit.com/project/5ea8030851543d00196ae33a/webhook/build/publish > /dev/null
